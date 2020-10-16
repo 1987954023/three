321676359cfbbc742a9941d5d76931d6a5cd44b6
1987954023 <1987954023@qq.com>
2020-10-16 18:29:54 +0800

    第一次

---
+++ index.js
@@ -0,0 +1,10 @@
+// $(function () {
+//     console.log(8)
+
+//     $.ajax({
+//         url: 'data.json',
+//         success: function (arr) {
+//             console.log(arr)
+//         }
+//     })
+// })
